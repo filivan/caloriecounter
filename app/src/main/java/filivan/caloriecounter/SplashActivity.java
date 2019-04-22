b5a6362377e9c33eadd8b27a066dffc0e28765cc
Filipp Nikonov <p.nikonov99@gmail.com>
2019-04-22 21:09:13 +0300

    first commit

---
+++ app/src/main/java/filivan/caloriecounter/SplashActivity.java
@@ -0,0 +1,18 @@
+package filivan.caloriecounter;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, StartScreen.class);
+        startActivity(intent);
+        finish();
+
+    }
+}
